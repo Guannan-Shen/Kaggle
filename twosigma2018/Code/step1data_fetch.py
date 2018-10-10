56930412380cbd8a6f3c47732b1ee5a9f07c964a
Guannan-Shen <guannan.shen.ai@gmail.com>
2018-10-09 21:00:12 -0600

    setup this proj but use Kaggle Kernel

---
+++ twosigma2018/Code/step1data_fetch.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import chain
+import random as rn
+from kaggle.competitions import twosigmanews
+# You can only call make_env() once, so don't lose it!
+env = twosigmanews.make_env()
+print('Done!')
